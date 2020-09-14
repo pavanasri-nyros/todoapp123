d3a1902d4a89f944fcc55da076ca856f139b9f6e
unknown <you@example.com>
2020-09-14 17:18:56 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="container">
+    <TodoInput />
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoInput from '@/components/TodoInput.vue';
+import TodoList from '@/components/TodoList.vue';
+export default {
+  name: 'App',
+  components: {
+    TodoInput,
+    TodoList,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
